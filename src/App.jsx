86b282f73d74e5cc910d474df0c6c972bc7da6ed
Ghazaleh Azimi <ghazalehazimi23@gmail.com>
2026-01-28 16:29:20 +0330

    Add new car products and update card styling
    Added Alfa Romeo Stelvio EV and Genesis GV60 Megma products with images. Refactored App.jsx to use a flex container for product cards and improved card styling in App.css, including hover effects and layout adjustments. Updated ProductCard to use h2 for product names.

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,48 @@ import React from "react";
 import ProductCard from "./component/ProductCard";
 import jeepImg from "./assets/JeepRecon.jpg"
 import KiaImg from "./assets/KiaTelluride.jpg"
-
+import AlfaImg from "./assets/Alfa.jpg"
+import GenImg from "./assets/Genesis.jpg"
 
 import './App.css'
 
 function App() {
   return (
-    <div>
-       <ProductCard
-       productImg={jeepImg}
-       name="Jeep Recon 2025"
-       price={54020}
-       />
-        <ProductCard
-       productImg={KiaImg}
-       name="Kia Telluride 2025"
-       price={85430}
-       />
-    </div>
+ <div className="pro-container">
+  <div className="card-pro">
+    <ProductCard
+      productImg={jeepImg}
+      name="Jeep Recon"
+      price={54020}
+    />
+  </div>
+
+  <div className="card-pro">
+    <ProductCard
+      productImg={KiaImg}
+      name="Kia Telluride"
+      price={85430}
+    />
+  </div>
+
+  <div className="card-pro">
+    <ProductCard
+      productImg={AlfaImg}
+      name="Alfa Romeo Stelvio EV"
+      price={70430}
+    />
+  </div>
+
+  <div className="card-pro">
+    <ProductCard
+      productImg={GenImg}
+      name="Genesis GV60 Megma"
+      price={90440}
+    />
+  </div>
+</div>
+
+
      
     
   );
--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 function ProductCard({ name, price, productImg}){
     return(
-        <div className="card-pro" >
+        <div >
             
             <img src={productImg} alt="Jeep Car"/>
-            <h1 className="text-pro">{name}</h1>
+            <h2 className="text-pro">{name}</h2>
             <h3 className="text-pro">Price $: {price}</h3>
             
         </div>
